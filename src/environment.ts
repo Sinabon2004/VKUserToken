b582e1f79809d3f9adcbc386f651f257a456d2a2
Chesnokov Artemii <115871031+Sinabon2004@users.noreply.github.com>
2025-05-22 22:31:28 +0300

    refactor(vite.config): updates for prod
    added new enviroment const + upd in vite.config

--- src/environment.ts
+++ src/environment.ts
@@ -2,6 +2,8 @@ export const APP_ID = 53_598_500 // your VK-ID App ID
 
 export const REDIRECT_URL = 'http://localhost'
 
+export const IS_LOCAL_DEV = true
+
 // const STATE = "lorem" // your state code
 
 // const CODE_VERIFIER = "lorem" // your code verifier
